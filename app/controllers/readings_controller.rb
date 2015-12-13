d1805bc5b2627c0c89221ace975b64ffe33a748c
gubala <tg.errors@gmail.com>
2015-12-13 10:44:36 +0100

    pm current to ug per m3

--- app/controllers/readings_controller.rb
+++ app/controllers/readings_controller.rb
@@ -8,9 +8,11 @@ class ReadingsController < ApplicationController
     temperature = Measurement.find_by_name('temperature')
     pm = Measurement.find_by_name('pm')
 
+    pm_value = (params[:pm].to_f / 1024) * 500
+
     Reading.create(value: params[:humidity].to_f, sensor: @sensor, measurement: humidity, time: Time.now)
     Reading.create(value: params[:temp].to_f, sensor: @sensor, measurement: temperature, time: Time.now)
-    Reading.create(value: params[:pm].to_f, sensor: @sensor, measurement: pm, time: Time.now)
+    Reading.create(value: pm_value, sensor: @sensor, measurement: pm, time: Time.now)
 
     head 200, content_type: "text/html"
   end
