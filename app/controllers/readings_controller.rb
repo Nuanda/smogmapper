1f154770eba11ac86ea6ec535274b810ba2135cb
gubala <tg.errors@gmail.com>
2015-12-13 08:47:27 +0100

    changed upload param name

--- app/controllers/readings_controller.rb
+++ app/controllers/readings_controller.rb
@@ -9,7 +9,7 @@ class ReadingsController < ApplicationController
     pm = Measurement.find_by_name('pm')
 
     Reading.create(value: params[:humidity].to_f, sensor: @sensor, measurement: humidity, time: Time.now)
-    Reading.create(value: params[:temperature].to_f, sensor: @sensor, measurement: temperature, time: Time.now)
+    Reading.create(value: params[:temp].to_f, sensor: @sensor, measurement: temperature, time: Time.now)
     Reading.create(value: params[:pm].to_f, sensor: @sensor, measurement: pm, time: Time.now)
 
     head 200, content_type: "text/html"
