d5d8b500d365209c5f90f63e71e11ea91f16edaf
gubala <tg.errors@gmail.com>
2015-12-14 11:22:29 +0100

    smarter seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,11 +10,16 @@ ms = [temperature, humidity, pm]
 sensor = Sensor.find(1000)
 (Sensor.all - [sensor]).each{ |s| s.destroy }
 
+puts Sensor.count
+
 fake_sensor_file = File.open('sensorLocation.csv').read
 fake_sensor_file.lines.each do |line|
   data = line.split(',')
   sensor = Sensor.create(id: data[0].to_i, name: data[0], long: data[1].to_f, lat: data[2].to_f, measurements: ms)
 end
+
+puts Sensor.count
+puts Reading.count
 
 11.times.each do |i|
   sensor_value_file = File.open("values#{i + 1}.csv").read
@@ -23,6 +28,8 @@ end
   	Reading.create(measurement: pm, sensor_id: data[0].to_i, time: Time.now - (i*15).minutes, value: data[1].to_f)
   end
 end
+
+puts Reading.count
 
 # 30.times.each do |i|
 #   Reading.create(measurement: temperature, sensor: sensor, time: Time.now - (i*15).minutes, value: (rand(60) - 20.0).to_f)
