97092c74df672a749542248b61af8af2ff822b41
gubala <tg.errors@gmail.com>
2015-12-14 11:10:47 +0100

    blocked external reading update

--- app/controllers/readings_controller.rb
+++ app/controllers/readings_controller.rb
@@ -2,17 +2,18 @@ class ReadingsController < ApplicationController
   layout false
 
   def show
-    @sensor = Sensor.find(params[:id])
-
-    humidity = Measurement.find_by_name('humidity')
-    temperature = Measurement.find_by_name('temperature')
-    pm = Measurement.find_by_name('pm')
-
-    pm_value = (params[:pm].to_f / 1024) * 500
-
-    Reading.create(value: params[:humidity].to_f, sensor: @sensor, measurement: humidity, time: Time.now)
-    Reading.create(value: params[:temp].to_f, sensor: @sensor, measurement: temperature, time: Time.now)
-    Reading.create(value: pm_value, sensor: @sensor, measurement: pm, time: Time.now)
+    # NOTE: blocked for demo mode
+    # @sensor = Sensor.find(params[:id])
+    #
+    # humidity = Measurement.find_by_name('humidity')
+    # temperature = Measurement.find_by_name('temperature')
+    # pm = Measurement.find_by_name('pm')
+    #
+    # pm_value = (params[:pm].to_f / 1024) * 500
+    #
+    # Reading.create(value: params[:humidity].to_f, sensor: @sensor, measurement: humidity, time: Time.now)
+    # Reading.create(value: params[:temp].to_f, sensor: @sensor, measurement: temperature, time: Time.now)
+    # Reading.create(value: pm_value, sensor: @sensor, measurement: pm, time: Time.now)
 
     head 200, content_type: "text/html"
   end
