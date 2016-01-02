6aac511938d91b4c0793353b42710d4c356ff239
Marek Kasztelnik <mkasztelnik@gmail.com>
2016-01-02 19:03:54 +0100

    Move seeds files into db/seeds directory

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,14 +7,14 @@ if Rails.env.development?
 
   Sensor.create(id: 1000, name: 'My own! My precious!', long: 19.959689, lat: 50.048504, measurements: ms)
 
-  fake_sensor_file = File.open('sensorLocation.csv').read
+  fake_sensor_file = File.open('db/seeds/sensorLocation.csv').read
   fake_sensor_file.lines.each do |line|
     data = line.split(',')
     Sensor.create(id: data[0].to_i, name: data[0], long: data[1].to_f, lat: data[2].to_f, measurements: ms)
   end
 
   11.times.each do |i|
-    sensor_value_file = File.open("values#{i + 1}.csv").read
+    sensor_value_file = File.open("db/seeds/values#{i + 1}.csv").read
     sensor_value_file.lines.each do |line|
       data = line.split(',')
       Reading.create(measurement: pm, sensor_id: data[0].to_i, time: Time.now - (i*15).minutes, value: data[1].to_f)
