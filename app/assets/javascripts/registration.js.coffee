b680eaa3ee3f157463c9d6cf5adee19377f45f84
gubala <tg.errors@gmail.com>
2016-01-09 14:20:23 +0100

    simple_form sensor registration - work in progress

--- app/assets/javascripts/registration.js.coffee
+++ app/assets/javascripts/registration.js.coffee
@@ -3,3 +3,9 @@ $ ->
     if $('#registration-form').length == 0
       $.get I18n.locale + '/locations/new', (data) ->
         $('#registration').html data
+
+  $('#sidebar-left').on('ajax:success', '#new_location', (e, data, status, xhr) ->
+    $('#registration').html xhr.responseText
+  ).on('ajax:error', '#new_location', (e, xhr, status, error) ->
+    $('#new_location').append "<p>TEMP ERROR</p>"
+  )
--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -3,7 +3,42 @@ class LocationsController < ApplicationController
 
   def new
     @location = Location.new
+    @location.sensor = Sensor.new
 
     render partial: 'locations/new'
+  end
+
+  def create
+    @sensor = Sensor.find_by_token(params[:location][:sensor][:token])
+
+    if @sensor
+      @location = Location.create(
+        location_params.merge(
+          sensor: @sensor,
+          registration_time: Time.now
+        )
+      )
+    else
+      @location = Location.new(location_params)
+      @location.sensor = Sensor.new
+      @location.sensor.errors.add(:token, :incorrect)
+
+      render partial: 'locations/new'
+    end
+  end
+
+  private
+
+  def create_params
+    params.require(:location).permit(:longitude, :latitude, :height, sensor: [:token, :name])
+  end
+
+  def location_params
+    filtered_params = create_params
+    {
+      longitude: filtered_params[:longitude],
+      latitude: filtered_params[:latitude],
+      height: filtered_params[:height]
+    }
   end
 end
